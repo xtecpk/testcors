fcca78865de2b1a9f8e30bd16aaaf48f333b4da0
Alihassan699 <yesalihassan@gmail.com>
2024-11-17 13:12:45 +0500

    location modal

--- src/pages/nieumatrix/damage-and-defects/modal/chip/ChipModal.tsx
+++ src/pages/nieumatrix/damage-and-defects/modal/chip/ChipModal.tsx
@@ -6,7 +6,7 @@ interface ChipModalProps {
 }
 
 const ChipModal: React.FC<ChipModalProps> = ({ show, onHide }) => {
-  // Always call useState first, before any early returns
+  // State hooks
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [parentId, setParentId] = useState<string>("");
@@ -62,166 +62,281 @@ const ChipModal: React.FC<ChipModalProps> = ({ show, onHide }) => {
     onHide(new MouseEvent("click")); // Close the modal
   };
 
+
   return (
-    <div
-      id="authentication-modal"
-      tabIndex={-1}
-      aria-hidden="true"
-      className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50 ml-4"
-      onClick={onHide} // Close modal when clicking on the overlay
-    >
+    <>
       <div
-        className="relative p-4 max-w-full w-[1100px] mx-auto rounded-lg bg-white shadow-md"
-        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside modal
+        id="authentication-modal"
+        tabIndex={-1}
+        aria-hidden="true"
+        className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50 ml-4"
+        onClick={onHide} // Close modal when clicking on the overlay
       >
-        {/* Modal content */}
-        <div className="text-start p-4">
-          <div className="row">
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">Title:</label>
-              <input
-                type="text"
-                className="form-control input"
-                placeholder="Enter Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
+        <div
+          className="relative p-4 max-w-full w-[1100px] mx-auto rounded-lg bg-white shadow-md"
+          onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside modal
+        >
+          {/* Modal content */}
+          <div className="text-start p-4">
+            <div className="row">
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">Title:</label>
+                <input
+                  type="text"
+                  className="form-control input"
+                  placeholder="Enter Title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">Description:</label>
+                <input
+                  type="text"
+                  className="form-control input"
+                  placeholder="Enter Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">ParentID</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setParentId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">Description:</label>
-              <input
-                type="text"
-                className="form-control input"
-                placeholder="Enter Description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">ParentID</label>
-              <select
-                value={parentId}
-                onChange={(e) => setParentId(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">Type</label>
-              <select
-                value={type}
-                onChange={(e) => setType(e.target.value)}
-                className="form-select "
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+            <div className="row">
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">Type</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setType(e.target.value)}
+                  className="form-select "
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">svgX</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setSvgx(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">svgy</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setSvgy(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">svgX</label>
-              <select
-                value={svgx}
-                onChange={(e) => setSvgx(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+            <div className="row">
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">svgPath</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setSvgPath(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">width</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setWidth(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">height</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setHeight(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">svgy</label>
-              <select
-                value={svgy}
-                onChange={(e) => setSvgy(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+            <div className="row">
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">vessel</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setVessel(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">deckId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setDeckId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">zoneId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setZoneId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">svgPath</label>
-              <select
-                value={svgPath}
-                onChange={(e) => setSvgPath(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+            <div className="row">
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">storageAreaId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setStorageAreaId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">positionId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setPositionId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-4">
+                <label className="inter fw-bold pt-2 pb-2">placementId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setPlacementId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">Width</label>
-              <select
-                value={width}
-                onChange={(e) => setWidth(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
+            <div className="row">
+              <div className="col-12">
+                <label className="inter fw-bold pt-2 pb-2">subZoneId</label>
+                <select
+                  value={parentId}
+                  onChange={(e) => setSubZoneId(e.target.value)}
+                  className="form-select"
+                  style={{ maxHeight: "150px", overflowY: "auto" }}
+                >
+                  {options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="col-4">
-              <label className="inter fw-bold pt-2 pb-2">Height</label>
-              <select
-                value={height}
-                onChange={(e) => setHeight(e.target.value)}
-                className="form-select"
-                style={{ maxHeight: "150px", overflowY: "auto" }}
-              >
-                {options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          {/* Add similar sections for other fields like `vessel`, `deckId`, etc. */}
+            {/* Save button */}
 
-          <div className="text-center mt-3">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleSave}
-            >
-              Save
-            </button>
+              <div className="text-center mt-3">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={handleSave}
+                >
+                  Save
+                </button>
+              </div>
+
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
