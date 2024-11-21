7367fb7b42bb21bf2f27d243c14c53bc8006cfdd
Alihassan699 <yesalihassan@gmail.com>
2024-11-21 08:43:41 +0500

    v1.0.4

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from 'ax
 
 // Create axios instance with base URL and headers
 const axiosInstance = axios.create({
-    baseURL: 'https://nieucore.com/api/', // API base URL
+    baseURL: 'https://nieucore.com/backend/api/', // API base URL
     headers: {
         'Content-Type': 'application/json',
     },
